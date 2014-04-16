86ad89d82fc744a9115bf241e286a0b494585aa6
Wubai Zhou <zhouwubai@gmail.com>
2014-04-15 20:05:25 -0400

    reorganize project

--- WebContent/js/mapUtil.js
+++ WebContent/js/mapUtil.js
@@ -2,417 +2,322 @@
  * @author zhouwubai
  */
 
-// 
-
 // user defined function
+function loadFinalEvents(id) {
 
-/**
- * function that change milliseconds to dataString
- */
-function changeMillisecondsToDateString(millionSeconds) {
-				var myDate = new Date(millionSeconds);
-				return myDate.toLocaleString().substring(0, 10);
+	var mapUtil = new FiuStorylineMapUtilObject();
+	
+	$.get("LoadFinalEventServlet", {
+		fileName : "allEvents" + id + ".out"
+	}, function(rtnData) {
+		var events = rtnData.events;
+//		testData = events;
+		mapUtil.setAllEvents(events);
+		$('.showAll').click(function() {
+			mapUtil.setAllEvents(events);
+			var markers = convertEvents2Markers(map, events);
+			mapUtil.setAllMarkers(markers);
+		});
+	});
+	
+
+	$.get("LoadFinalEventServlet", {
+		fileName : "finalResult" + id + ".out"
+	}, function(rtnData) {
+
+		var events = rtnData.events;
+		mapUtil.setDominateEvents(events);
+
+		$('.showDom').click(function() {
+			var markers = convertEvents2Markers(map, events);
+			mapUtil.setDominateMarkers(markers);
+		});
+
+		$('.showStorylinePath').click(function() {
+			var storylineEvents = chooseNodesOfMainStoryline(events);
+			updateContentOfStoryPanel(storylineEvents);
+			var markers = convertEvents2Markers(map, storylineEvents);
+			mapUtil.setStorylineMarkers(markers);
+			mapUtil.displayStorylinePoly(map, storylineEvents);
+		});
+
+	}, 'json');
+
 }
 
-/**
- * function that choose nodes belongs to storyline
- * @param events
- * @returns {Array}
- */
-function chooseNodesOfMainStoryline(events)
-{
+function changeMillisecondsToDateString(millionSeconds) {
+	var myDate = new Date(millionSeconds);
+	return myDate.toLocaleString().substring(0, 10);
+}
+
+function chooseNodesOfMainStoryline(events) {
 	var rtn = [];
-	for(var i = 0; i < events.length; i++){
-		if(events[i].isMainEvent)
+	for (var i = 0; i < events.length; i++) {
+		if (events[i].isMainEvent)
 			rtn.push(events[i]);
 	}
-	
+
 	return rtn;
 }
 
 
-/**
- * distance between a marker and event
- * @param marker
- * @param event
- * @returns
- */
-function distOfMarkerAndEvent(marker,event){
-	var latDiff = marker.getPosition().lat() - event.latlng.latitude;
-	var longDiff = marker.getPosition().lng() - event.latlng.longtitude;
-	return Math.sqrt(Math.pow(latDiff,2) + Math.pow(longDiff,2));
+function pickDefaultParam(a, defaultVal) {
+	return a = typeof a !== 'undefined' ? a : defaultVal;
 }
 
 
-/**
- * choose neighbor events of a marker
- * @param marker
- * @param events candidate events.
- * @param radius
- * @returns {Array} array that contains events that near marker
- */
-function chooseMarkerNeighbors(marker,events,radius){
-	var rtnEvents = [];
-	
-	for(var i = 0; i < events.length; i++){
-		if(distOfMarkerAndEvent(marker, events[i]) <= radius)
-			rtnEvents.push(events[i]);
-	}
-	return rtnEvents;
-}
+//some function related google map
+function getInfoWindowContent(event) {
+	var content = "<div id='infoDiv'>"
+			+ "<br> location: <a target = '_blank' href = '"
+			+ event.eventURL + "'> " + event.eventLocation + "</a>"
+			+ "<br> Latitude: " + event.latlng.latitude
+			+ "<br> Longtitude: " + event.latlng.longtitude + "<br> Info: "
+			+ event.eventContent + "<br> Date: "
+			+ changeMillisecondsToDateString(event.eventDate) + "</div>";
 
-/**
- * if a parameter is not set, then choose a defaultVal
- * @param a
- * @param defaultVal
- * @returns
- */
-function pickDefaultParam(a, defaultVal)
-{
-  return a = typeof a !== 'undefined' ? a : defaultVal;
+	return content;
 }
 
 
-/**
- * convert events' location to a MVCArray
- * @param events
- * @returns {google.maps.MVCArray}
- */
-function eventsToMVCArray(events){	
+function attachInfoWindow(map, marker, event) {
+
+	var infowindow = new google.maps.InfoWindow({
+		content : getInfoWindowContent(event),
+		size : new google.maps.Size(50, 50)
+	});
+	// add listener to this marker
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(map, marker);
+	});
+}
+
+
+
+function convertEventLatLng(event) {
+	return new google.maps.LatLng(event.latlng.latitude,
+			event.latlng.longtitude);
+};
+
+
+function eventsToMVCArray(events) {
 	var rtn = [];
-	for(var i = 0; i < events.length; i++){
-		var tmp = new google.maps.LatLng(events[i].latlng.latitude,events[i].latlng.longtitude);
-		rtn.push(tmp);
+	for (var i = 0; i < events.length; i++) {
+		rtn.push(convertEventLatLng(event[i]));
 	}
 	return new google.maps.MVCArray(rtn);
 }
 
 
-/**
- * 
- * @param event
- */
-function updateContentOfStoryPanel(event){
-	
+
+function convertEvents2Markers(map, events) {
+
+	var rtnMarkers = [];
+	for (var i = 0; i < events.length; i++) {
+		var markerLatLng = convertEventLatLng(events[i]);
+		var marker = new google.maps.Marker({
+			position : markerLatLng,
+			map : map,
+			title : events[i].eventContent,
+			zIndex : google.maps.Marker.MAX_ZINDEX
+		});
+		
+		marker.event = events[i]; // relate event with this marker.
+		rtnMarkers.push(marker);
+	}
+	return rtnMarkers;
+}
+
+
+
+
+
+//for hidable
+function updateContentOfStoryPanel(events) {
+
 	var storyPanel = {};
-	leftN = Math.ceil(event.length / 2);
-	eventL = event.slice(0,leftN);
-	eventR = event.slice(leftN);
-	
+	var leftN = Math.ceil(events.length / 2);
+	var eventL = events.slice(0, leftN);
+	var eventR = events.slice(leftN);
+
 	var leftHtml = "";
-	for(var i=0;i<eventL.length;i++){
-		leftHtml += "<li class='eventID_'"+event[i].id+">"+(i+1)+". <a href=#>"+event[i].eventContent+"</a></li>";
+	for (var i = 0; i < eventL.length; i++) {
+		leftHtml += "<li class='eventID_'" + events[i].id + ">" + (i + 1)
+				+ ". <a href=#>" + events[i].eventContent + "</a></li>";
 	}
-	storyPanel.leftHtml = '<ul style="list-style-type:none">'+leftHtml+'</ul>';
-	
+	storyPanel.leftHtml = '<ul style="list-style-type:none">' + leftHtml
+			+ '</ul>';
+
 	var rightHtml = "";
-	for(var i=0;i<eventR.length;i++){
-		rightHtml += "<li class='eventID_'"+event[i+leftN].id+">"+(i+leftN+1)+". <a href=#>"+event[i+leftN].eventContent+"</a></li>";
+	for (var i = 0; i < eventR.length; i++) {
+		rightHtml += "<li class='eventID_'" + events[i + leftN].id + ">"
+				+ (i + leftN + 1) + ". <a href=#>"
+				+ events[i + leftN].eventContent + "</a></li>";
 	}
-	storyPanel.rightHmtl = '<ul style="list-style-type:none">'+rightHtml+'</ul>';
-	
+	storyPanel.rightHmtl = '<ul style="list-style-type:none">' + rightHtml
+			+ '</ul>';
+
 	$('.storyline_left').html(storyPanel.leftHtml);
 	$('.storyline_right').html(storyPanel.rightHmtl);
 }
 
 
 
-function setHidableHeight(height){
-	$('.hidable').css('height',height);
-	$('.hidable').css('bottom',height);
-	$('.storylinePanel').css('height',height);
-	$('.storyline_left').css('height',height);
-	$('.storyline_right').css('height',height);
+function setHidableHeight(height) {
+	$('.hidable').css('height', height);
+	$('.hidable').css('bottom', height);
+	$('.storylinePanel').css('height', height);
+	$('.storyline_left').css('height', height);
+	$('.storyline_right').css('height', height);
 }
 
 
-/**
- * toggle hidable area.
- */
-function clickHidableToggler(){
+function clickHidableToggler() {
 	var current = $('.hidable_toggler_btn em').attr('class');
 	var flag = (current.trim() == "control_arrow_down");
-	if(flag){
+	if (flag) {
 		$('.hidable_toggler_btn em').removeClass("control_arrow_down");
 		$('.hidable_toggler_btn em').addClass("control_arrow_up");
 		setHidableHeight(0);
-	}else{
+	} else {
 		$('.hidable_toggler_btn em').removeClass("control_arrow_up");
 		$('.hidable_toggler_btn em').addClass("control_arrow_down");
 		setHidableHeight(250);
 	}
 }
-//user defined object
-// Google Map Util Object
 
 
-function FiuStorylineMapUtilObject(){
-	
+
+// singleton InfoWindow
+var infowindow = new google.maps.InfoWindow({
+	size : new google.maps.Size(50, 50)
+});
+function FiuStorylineMapUtilObject() {
+
 	var self = this;
 	
+	self.allEvents = [];
+	self.dominateEvents = [];
+	self.storylineEvents = [];
+
+	self.allMarkers = [];
+	self.dominateMarkers = [];
 	self.storylineMarkers = [];
 	self.mediumStorylineMarkers = [];
-	
-	
+
 	self.storylinePoly = new google.maps.Polyline();
 	self.mediumStorylinePoly = [];
-	self.heatMap = new google.maps.visualization.HeatmapLayer({radius:20});
 	
-	//singleton InfoWindow
-	self.infowindow = new google.maps.InfoWindow({size : new google.maps.Size(50, 50) });
-	self.markerCluster = {};
-	
-	//default value
+	// default value
 	var lineSymbol = {
-			path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+		path : google.maps.SymbolPath.FORWARD_CLOSED_ARROW
 	};
-	
-	self.polyOptions = {
-		    strokeColor: '#000000',
-		    strokeOpacity: 1.0,
-		    strokeWeight: 3,
-		    icons: [{
-		        icon: lineSymbol,
-		        offset: '100%'
-		      }]
-		  };
-	self.redPolyOptions = {
-		    strokeColor: '#FF0000',
-		    strokeOpacity: 1.0,
-		    strokeWeight: 3,
-		    icons: [{
-		        icon: lineSymbol,
-		        offset: '100%'
-		      }]
-		  };
-	
-	self.markerClusterOptions = {
-			gridSize: 50, maxZoom: 15
-	};
-	
-	self.iconURL = "http://openclipart.org/people/mightyman/green.svg";
-	
-	
-	FiuStorylineMapUtilObject.prototype.setMarkers = function(map,events){
-		
-		//before loop, we set variable index to 0, set previous markers unattached to map
-		self.clearMarkers(self.storylineMarkers);
-		
-		for ( var i = 0; i < events.length; i++) {
-			var markerLatLng = new google.maps.LatLng(
-					events[i].latlng.latitude,
-					events[i].latlng.longtitude);
-			var marker = new google.maps.Marker({ position : markerLatLng,
-			map : map,
-			title : events[i].eventContent,
-			//icon: "http://openclipart.org/people/mightyman/green.svg",
-			zIndex: google.maps.Marker.MAX_ZINDEX});
-			
-			marker.event = events[i];  //relate event with this marker.
-			self.storylineMarkers.push(marker);
-			
-			updateContentOfStoryPanel(events);
-			
-			self.addListenerByClickMarker(marker);
-			self.addListenerBydbClickMarkerToZoomIn(marker);		
-		}
-	};
-	
-	
-	
-	FiuStorylineMapUtilObject.prototype.setLayerTwoMarker = function(map,events){
-		
-		self.clearMarkers(self.mediumStorylineMarkers);
-		console.log(events);
-		for ( var i = 0; i < events.length; i++) {
-			var markerLatLng = new google.maps.LatLng(
-					events[i].latlng.latitude,
-					events[i].latlng.longtitude);
-			var marker = new google.maps.Marker({ position : markerLatLng,
-			map : map,
-			title : events[i].eventContent,
-			icon: "http://openclipart.org/people/mightyman/green.svg",
-			zIndex: google.maps.Marker.MAX_ZINDEX});
-			
-			marker.event = events[i];  //relate event with this marker.
-			self.mediumStorylineMarkers.push(marker);
-			
-			self.addListenerByClickMarker(marker);
-			//var mc = new MarkerClusterer(map, this.mediumStorylineMarkers, this.markerClusterOptions);
-			
-		}
-	};
-	
-	//to avoid issues caused by closure
-	FiuStorylineMapUtilObject.prototype.addListenerByClickMarker = function(marker){
-		google.maps.event.addListener(marker, 'click', function(event) {
-//			console.log(1);
-//			map.panTo(marker.getPosition());
-			console.log(marker.event);
-			self.infowindow.setContent(self.getInfoWindowContent(marker.event));
-			self.infowindow.open(map,marker);
-		});
-	};
-	
-	
-	// its better to set those function as private
-	FiuStorylineMapUtilObject.prototype.addListenerBydbClickMarkerToZoomIn = function(marker){
-		google.maps.event.addListener(marker, 'dblclick', function(event) {
-//			console.log(2);
-			map.setZoom(6);
-			map.setCenter(marker.getPosition());
 
-			self.clearPoly(self.storylinePoly);
-			self.clearPolys(self.mediumStorylinePoly);
-			//refThis.displayPoly(map,neighbor,refThis.mediumStorylinePoly,refThis.redPolyOptions);
-			
-			
-			$.get("loadLocalSteinerTree",{
-				disasterID:disasterID,
-				eventID:marker.event.id
-			},function(data){
-				
-				var arcs = data.arcs;
-				arcEventsMap = {};
-				for(var i = 0; i < arcs.length; i++){
-					if(arcEventsMap[arcs[i].input] == undefined){
-						arcEventsMap[arcs[i].input] = self.mapAllEvents[arcs[i].input];
-					}
-					if(arcEventsMap[arcs[i].output] == undefined){
-						arcEventsMap[arcs[i].output] = self.mapAllEvents[arcs[i].output];
-					}
-				}
-				
-				arcEvents = [];
-				for(var e in arcEventsMap){
-					arcEvents.push(arcEventsMap[e]);
-				}
-				
-				console.log(data);
-				console.log(arcEventsMap);
-				console.log(arcEvents);
-				self.setLayerTwoMarker(map,arcEvents);
-				self.displayPolys(map,arcEventsMap,arcs,self.redPolyOptions);
-				
-			});
-		});
+	self.polyOptions = {
+		strokeColor : '#000000',
+		strokeOpacity : 1.0,
+		strokeWeight : 3,
+		icons : [ {
+			icon : lineSymbol,
+			offset : '100%'
+		} ]
 	};
 	
+	self.redPolyOptions = {
+		strokeColor : '#FF0000',
+		strokeOpacity : 1.0,
+		strokeWeight : 3,
+		icons : [ {
+			icon : lineSymbol,
+			offset : '100%'
+		} ]
+	};
+
+	self.iconURL = "http://openclipart.org/people/mightyman/green.svg";
+
 	
-	
-	
-	FiuStorylineMapUtilObject.prototype.clearMarkers = function(markers){
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(null);
-        }
-        markers = [];
-    };
-    
-    
-    FiuStorylineMapUtilObject.prototype.clearPoly = function(poly){
-    	poly.setMap(null);
-    	poly.getPath().clear();
-    };
-    
-    
-    FiuStorylineMapUtilObject.prototype.clearPolys = function(polys){
-    	for(var i = 0; i < polys.length;i++){
-    		polys[i].setMap(null);
-        	polys[i].getPath().clear();
-    	}
-    };
-    
-	
-    FiuStorylineMapUtilObject.prototype.attachInfoWindow = function(map,marker,event) {
-		
-		var infowindow = new google.maps.InfoWindow({ 
-			content : self.getInfoWindowContent(event),
-			size : new google.maps.Size(50, 50) });
-		//add listener to this marker
-		google.maps.event.addListener(marker, 'click', function() {
+	// to avoid issues caused by closure
+	FiuStorylineMapUtilObject.prototype.addListenerByClickMarker = function(marker) {
+		google.maps.event.addListener(marker, 'click',function(event) {
+			infowindow.setContent(getInfoWindowContent(marker.event));
 			infowindow.open(map, marker);
 		});
 	};
+
 	
-	
-	
-	
-	FiuStorylineMapUtilObject.prototype.convertEventLatLng = function(event)
-	{
-		return new google.maps.LatLng(event.latlng.latitude,event.latlng.longtitude);
+	FiuStorylineMapUtilObject.prototype.clearAll = function(){
+		self.clearMarkers(self.allMarkers);
+		self.clearMarkers(self.dominateMarkers);
+		self.clearMarkers(self.storylineMarkers);
+		self.clearPoly(self.storylinePoly);
 	};
 	
-	FiuStorylineMapUtilObject.prototype.displayPoly = function(map,events,poly,polyOptions){
+	
+	FiuStorylineMapUtilObject.prototype.clearMarkers = function(markers) {
+		for (var i = 0; i < markers.length; i++) {
+			markers[i].setMap(null);
+		}
+		markers = [];
+	};
+
+	
+	FiuStorylineMapUtilObject.prototype.clearPoly = function(poly) {
+		poly.setMap(null);
+		poly.getPath().clear();
+	};
+
+
+	FiuStorylineMapUtilObject.prototype.displayStorylinePoly = function(map, events,polyOptions) {
+
+		self.clearPoly(self.storylinePoly);
 		
 		polyOptions = pickDefaultParam(polyOptions, self.polyOptions);
-		poly.setOptions(polyOptions);
-		poly.setMap(map);
-		
-		var path = poly.getPath();
-		for(var i = 0; i < events.length; i++){
-			path.push(self.convertEventLatLng(events[i]));
+		self.storylinePoly.setOptions(polyOptions);
+		self.storylinePoly.setMap(map);
+
+		var path = self.storylinePoly.getPath();
+		for (var i = 0; i < events.length; i++) {
+			path.push(convertEventLatLng(events[i]));
 		}
-		
 	};
-	
-	
-		FiuStorylineMapUtilObject.prototype.displayPolys = function(map,events,arcs,polyOptions){
-		
-		for(var i = 0; i < arcs.length; i ++){
-			
-			var poly = new google.maps.Polyline();
-			polyOptions = pickDefaultParam(polyOptions, self.polyOptions);
-			poly.setOptions(polyOptions);
-			poly.setMap(map);
-			
-			var path = poly.getPath();
-			path.push(self.convertEventLatLng(events[arcs[i].input]));
-			path.push(self.convertEventLatLng(events[arcs[i].output]));
-			
-			self.mediumStorylinePoly.push(poly);
-		}
-		
-	};
-	
-	
-	
-	//setter and getters
-	FiuStorylineMapUtilObject.prototype.setPolyOptions = function(polyOptions)
-	{
-		self.polyOptions = polyOptions;
-	};
-	
-	//set finalResult 
-	FiuStorylineMapUtilObject.prototype.setEvents = function(events){
-		self.events = events;
-	};
-	
-	
-	FiuStorylineMapUtilObject.prototype.setAllEvents = function(events){
+
+
+	//setters
+	FiuStorylineMapUtilObject.prototype.setAllEvents = function(events) {
 		self.allEvents = events;
-		self.mapAllEvents = {};
-		for(var i = 0; i < events.length; i++){
-			self.mapAllEvents[events[i].id] = events[i];
+	};
+	
+	FiuStorylineMapUtilObject.prototype.setDominateEvents = function(events){
+		self.dominateEvents = events;
+	};
+	
+	FiuStorylineMapUtilObject.prototype.setStorylineEvents = function(events){
+		self.storylineEvents = events;
+	};
+	
+	FiuStorylineMapUtilObject.prototype.setAllMarkers = function(markers){
+		self.allMarkers = markers;
+		self.addClickListener(markers);
+	};
+	
+	FiuStorylineMapUtilObject.prototype.setDominateMarkers = function(markers){
+		self.dominateMarkers = markers;
+		self.addClickListener(markers);
+	};
+	
+	FiuStorylineMapUtilObject.prototype.setStorylineMarkers = function(markers){
+		self.storylineMarkers = markers;
+		self.addClickListener(markers);
+	};
+	
+	FiuStorylineMapUtilObject.prototype.setMediumStorylineMarkers = function(markers){
+		self.mediumStorylineMarkers = markers;
+		self.addClickListener(markers);
+	};
+	
+	FiuStorylineMapUtilObject.prototype.addClickListener = function(markers){
+		for(var i = 0; i < markers.length; i++){
+			self.addListenerByClickMarker(markers[i]);
 		}
-		console.log(self.mapAllEvents);
 	};
-	
-	FiuStorylineMapUtilObject.prototype.getPolyOptions = function()
-	{
-		return self.polyOptions;
-	};
-	
-	FiuStorylineMapUtilObject.prototype.getInfoWindowContent = function(event){		
-		var content = "<div id='infoDiv'>"
-			+ "<br> location: <a target = '_blank' href = '" + event.eventURL + "'> "
-			+ event.eventLocation + "</a>" + "<br> Latitude: "
-			+ event.latlng.latitude + "<br> Longtitude: "
-			+ event.latlng.longtitude + "<br> Info: " + event.eventContent
-			+ "<br> Date: "
-			+ changeMillisecondsToDateString(event.eventDate)
-			+ "</div>";
-	
-		return content;
-	};
-	
 };
